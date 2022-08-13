95cf9658c765d001957d24a2e0373c260695f93b
Jeff Schaefer <blkhwks19@yahoo.com>
2022-08-12 21:21:53 -0500

    Mongo up and running

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 app.use(express.json());
+
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log(`Connected to database: ${process.env.DATABASE_URL}`));
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
